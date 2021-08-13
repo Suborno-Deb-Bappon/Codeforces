b3b3ba73db8d3c55f5d44d5c1f7fa767d9277197
Suborno Deb Bappon <86287224+Suborno-Deb-Bappon@users.noreply.github.com>
2021-08-13 23:06:21 +0600

    Add files via upload

---
+++ Required Remainder.py
@@ -0,0 +1,10 @@
+n = int(input())
+def func(a,b,c):
+    if c-(c%a)+b <= c:
+        return c-(c%a)+b
+    else:
+         return c-(c%a)+b-a
+for i in range(n):
+    x,y,n = [int(x) for x in input().split()]
+    d = func(x,y,n)
+    print(d)
